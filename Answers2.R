69599d9a821aac78064f8de1eb23285e7fcc569a
Yunus Emre <yunusemrerdam@gmail.com>
2018-12-09 11:45:52 +0100

    Answer to second question

--- Answers2.R
+++ Answers2.R
@@ -29,3 +29,56 @@ var2 <- as.Date(c('2010-11-1','2008-3-25','2007-3-14'))
 employ.data <- data.frame(names, var1, var2)
 
 example_answer1 <- tidy_df(employ.data, "var")
+
+
+# Question 2 ------------------------------------------------------------------------------------------------------
+
+#' Get the Jane Austen data
+#'
+#' It will attempt to install the right package for you. If it does not work,
+#'   try to install it manually.
+#'
+#' @return A data frame with Jane Austen texts, one line per row
+get_jane_austen_data <- function(){
+  
+  tryCatch({library(gutenbergr)}, error = function(e){install.packages("gutenbergr")})
+  library(gutenbergr)
+  
+  austen_text <- gutenberg_works(author == "Austen, Jane") %>% 
+    gutenberg_download(meta_fields = "title") %>% mutate(id = row_number(gutenberg_id))
+  assign("austen_text", austen_text, envir=.GlobalEnv)
+  invisible()
+}
+
+get_jane_austen_data()
+
+# extract_possible_names 
+
+#' Extracts all words that start with a capital letter from texts
+#'
+#' @param data dataframe  
+#'
+#' @return a data frame with three columns; “text_id” a unique identifier, “id” from the original data frame
+#' and “name”, extracted name for all rows which are extracted names.
+
+extract_possible_names <- function(data){
+  extracted <- data
+  
+  extracted <- data.frame(text = gsub('[[:punct:] ]+',' ',extracted$text),
+                          id = extracted$id, title= extracted$title)
+  
+  extracted <- mutate(extracted, 
+                      name = unlist(sapply(
+                        str_extract_all(extracted$text, '\\b[A-Z]\\w+'), paste, collapse = ' ')))
+  
+  extracted <- extracted[!(is.na(extracted$name) | extracted$name==""), ]
+  
+  s <- strsplit(extracted$name, split = " ")
+  extracted <- data.frame(text_id = 
+                            rep(extracted$id, sapply(s, length)), name = unlist(s))
+  
+  extracted <- mutate(extracted, id = 1:nrow(extracted))
+  
+} 
+
+answer2 <- extract_possible_names(austen_text)  
